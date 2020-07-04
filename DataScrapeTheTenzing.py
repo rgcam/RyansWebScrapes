b77dc0e2ef11bb3b1c3fd5eb10e6725060ca4c63
Ryan Cameron <45379493+rgcam@users.noreply.github.com>
2020-07-04 17:04:52 -0500

    Add files via upload

---
+++ DataScrapeTheTenzing.py
@@ -0,0 +1,88 @@
+from urllib.request import urlopen, Request
+import requests
+from bs4 import BeautifulSoup as soup
+import re
+import time
+import datetime
+
+def ScrapeWebpage():
+    url = Request('https://www.thetenzingapts.com/floorplans.aspx', headers={'User-Agent': 'Mozilla/5.0'})
+
+    #opening up connection, getting page
+    uClient = urlopen(url)
+    page_html = uClient.read()
+    uClient.close()
+
+    #parsing page
+    page_soup = soup(page_html, "html.parser")
+    pre_sub_soup = page_soup.findAll('tbody', {"class":"floorplan-details"})
+
+    sub_soup = soup(pre_sub_soup[2].prettify(), "html.parser") # 0 = 1Br, 1 = 2Br, 2 = 3Rr
+    # print(sub_soup)
+
+    #---------rent---------------------#
+    floorplans_all = sub_soup.findAll('td', {"data-label":"Rent"})
+    floorplan_soup = soup(floorplans_all[0].prettify(), "html.parser")
+    rent_span = floorplan_soup.findAll('span', {"class":"sr-only"})
+
+    fp_all_details = [span.next_sibling.string.strip() for span in sub_soup.findAll(class_='sr-only')]
+
+    # print(row_details)
+
+
+    #---------available----------------#y
+    results = page_soup.findAll('td', {"data-label":"Availability"})
+
+    string = "id=\"3"
+
+    i = 0
+    # for pos in results:
+    #     if string in str([pos]):
+    #         print(i)
+    #     i = + 1
+    availbility_int_list = []
+    while i < len(results):
+        if string in str(results[i]):
+            availbility_int_list.append(i)
+            i = i + 1
+        else:
+            i = i + 1
+
+    # print(availbility_int_list)
+
+    scraped_availability_list = []
+    for row in availbility_int_list:
+        a = soup(results[row].prettify(), "html.parser")
+
+        available_found = a.findAll("span", {"class":"available-fp"})
+
+        available = str(available_found[0])
+        available_list = re.findall(r'\d+', available)
+
+        scraped_availability_list.append(str(available_list[0]) + " Available")
+
+    #----------------------------------#
+
+    # print(len(row_details))
+    floorplan_count = int(len(fp_all_details) / 6) # each row has 6 items in row_details
+    # print(floorplan_count)
+
+    if floorplan_count == 1:
+        fp_all_details[5] = scraped_availability_list[0]
+
+    if floorplan_count == 2:
+        fp_all_details[5] = scraped_availability_list[0]
+        fp_all_details[11] = scraped_availability_list[1]
+
+    if floorplan_count == 3:
+        fp_all_details[5] = scraped_availability_list[0]
+        fp_all_details[11] = scraped_availability_list[1]
+        fp_all_details[17] = scraped_availability_list[2]
+
+    # time.wait(1)
+    
+    return fp_all_details
+
+# print(ScrapeWebpage)
+
+# print(15 - datetime.datetime.now().minute % 15)
---
+++ botbot.py
@@ -0,0 +1,136 @@
+# bot.py
+import os
+import discord
+import asyncio
+from dotenv import load_dotenv
+import time
+import DataScrapeTheTenzing
+import datetime
+from discord import Webhook as wh
+from discord import RequestsWebhookAdapter
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+WH_TOKEN = os.getenv('SPIDEY_BOT_WEBHOOK_TOKEN')
+WH_ID = os.getenv('SPIDEY_BOT_WEBHOOK_ID')
+
+client = discord.Client()
+webhook = wh.partial(WH_ID, WH_TOKEN, adapter=RequestsWebhookAdapter())
+
+async def background_scrape_timer():
+    print("in bg task")
+    await client.wait_until_ready()
+    counter = 0
+    channel = discord.Object(id=GUILD)
+    while not client.is_closed:
+        print("in while")
+        counter += 1
+        await webhook.send("The Tenzing 3 Bedroom Availability:")
+        await asyncio.sleep(60) # task runs every 60 seconds
+        print("asleep")
+        
+        # rent = ""
+        # webhook.send("BotBot starting up...")
+        # while True:
+        #     if datetime.datetime.now().minute % 15 == 0: 
+        #         stats = DataScrape.ScrapeWebpage()
+        #         await webhook.send("The Tenzing 3 Bedroom Availability:")
+        #         await webhook.send("Floor Plan: " + stats[0])
+        #         await webhook.send("Bed/Bath: " + stats[1])
+        #         await webhook.send("Rent: " + stats[3] + stats[4])
+        #         await webhook.send(stats[5])
+
+        #         if rent != stats[3] + stats[4]:
+        #             await webhook.send("@Everyone rent has changed!")
+
+        #         else:
+        #             continue
+
+        #         rent = stats[3] + stats[4]
+        #     else: 
+        #         continue
+        #     time.sleep((15 - datetime.datetime.now().minute % 15) * 60)
+
+@client.event
+async def on_ready():
+    """
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = '\n -'.join([member.name for member in guild.members])
+    print('\n Guild Members:\n -' + members)
+    """
+    print(str(client.user.name) + " is connected!") 
+
+# @client.command
+# async def mention(self, ctx, member : discord.Member):
+#     await ctx.send(f"{member}")  
+
+@client.event
+async def on_message(message):\
+
+    
+    if message.author == client.user:
+        return
+
+    if message.content == "Hello" or message.content == "hello":
+        greeting = "Hello!"
+        await message.channel.send(greeting)
+    
+    if message.content == "Availability" or message.content == "availability":
+        stats = DataScrapeTheTenzing.ScrapeWebpage()
+        webhook.send("The Tenzing 3 Bedroom Availability:" + 
+                        "\nFloor Plan: " + stats[0] + 
+                        "\nBed/Bath: " + stats[1] +
+                        "\nRent: " + stats[3] + stats[4] + 
+                        "\n" +stats[5])
+   
+
+_loop = asyncio.get_event_loop()
+
+async def my_background_task():
+    webhook.send("This is \n a test")
+    rent = ""
+    while True:
+        if datetime.datetime.now().minute % 15 == 0: 
+            stats = DataScrapeTheTenzing.ScrapeWebpage()
+            webhook.send("__________________________________")
+            webhook.send("The Tenzing 3 Bedroom Availability:" + 
+                        "\nFloor Plan: " + stats[0] + 
+                        "\nBed/Bath: " + stats[1] +
+                        "\nRent: " + stats[3] + stats[4] + 
+                        "\n" +stats[5])
+
+            # webhook.send("Floor Plan: " + stats[0])
+            # webhook.send("Bed/Bath: " + stats[1])
+            # webhook.send("Rent: " + stats[3] + stats[4])
+            # webhook.send(stats[5])
+
+            time.sleep(60)
+
+            if rent != stats[3] + stats[4] and rent != "":
+                myid = os.getenv('RGCAM')
+                webhook.send("<@" + myid + "> rent has changed!")
+
+            else:
+                continue
+
+            rent = stats[3] + stats[4]
+        else: 
+            continue
+        time.sleep((15 - datetime.datetime.now().minute % 15) * 60)
+
+
+# client.loop.create_task(background_scrape_timer())
+client.loop.create_task(my_background_task())
+client.run(TOKEN)
+# time.wait(5)
+
